57f1c16e4c068b94774e2c1ddba7364b27068c25
MIGUEL <miguel92grnd@gmail.com>
2015-01-25 22:21:51 +0100

    Borradas pruebas

--- test.py
+++ test.py
@@ -5,17 +5,14 @@ from pruebadriveiv import Test
 
 class InsertTestCase(unittest.TestCase):
     def setUp(self):
-    	print "zapatos"
         self.testbed = testbed.Testbed()
         self.testbed.activate()
         self.testbed.init_urlfetch_stub()
 
     def tearDown(self):
-    	print "zapatos"
         self.testbed.deactivate()
 
     def test(self):
-	print "zapatos"
 	test = Test()
 	response = test.Inserta('OK')
 	self.assertEqual(response, 'OK')
