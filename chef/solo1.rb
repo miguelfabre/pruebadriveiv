326277d58ddb26e62e61d0ab8e183af163b4a512
miguelfabre <miguel92grnd@gmail.com>
2015-02-01 22:12:43 +0100

    Update solo1.rb

--- chef/solo1.rb
+++ chef/solo1.rb
@@ -1,6 +1,6 @@
 # PATH TO CHEF debe ser la ruta relativa a la carpeta de chef (llamada "chef").
   
-file_cache_path "/PATH/TO/CHEF"
-cookbook_path "/PATH/TO/CHEF/cookbooks"
-json_attribs "/PATH/TO/CHEF/node1.json"
+file_cache_path "/etc/chef"
+cookbook_path "/etc/chef/cookbooks"
+json_attribs "/etc/chef/node1.json"
 
